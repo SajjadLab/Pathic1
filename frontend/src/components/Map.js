78873b9777f40aae6163677bf685df6e2ddb35f1
SajjadHub <alkazzazsajjad@gmail.com>
2022-08-12 09:56:17 -0700

    tweaking while i figure things out

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,14 +1,16 @@
 // Cesium imports
 import { Viewer, GeoJsonDataSource, ScreenSpaceEvent, ScreenSpaceEventHandler, Scene } from "resium";
-import { Entity, Color, NearFarScalar, DistanceDisplayCondition, ScreenSpaceEventType, BoundingSphere, VRTheWorldTerrainProvider } from "cesium";
+import { Ion, IonResource, Entity, Color, NearFarScalar, DistanceDisplayCondition, ScreenSpaceEventType, BoundingSphere, ArcGISTiledElevationTerrainProvider, createWorldTerrain, ShadowMode } from "cesium";
 // React imports
 import { Component } from "react";
 // Bootstrap imports
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+//import Container from "react-bootstrap/Container";
+//import Row from "react-bootstrap/Row";
+//import Col from "react-bootstrap/Col";
 
 // import PathicNavbar from "./Navbar.js"
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhOGJjN2E4MC0wZmFmLTRlMjEtYWMwZi1lZjRjMWU2NGRkNDciLCJpZCI6MTA0NTUyLCJpYXQiOjE2NjAyOTUxMDF9.4NgroU9rZOF3EWRyWlc4UYXZ-8g-TGTjbCyiWMf5FIg";
 
 class Map extends Component {
 
@@ -43,63 +45,63 @@ class Map extends Component {
     };
   };
 
+
   render () {
     return (
-      <Container fluid>
-        <Row><Col>Beep</Col></Row>
-        <Row>
-          <Col>
-            <Viewer
-              ref={ref => { this.viewerComponent = ref; }}
-              //full
-              resize
-              animation={false}
-              timeline={false}
-              vrButton={true}
-              // terrainProvider={ new VRTheWorldTerrainProvider({               url: 'https://www.vr-theworld.com/vr-theworld/tiles1.0.0/73/' }) }
-            >
-              {/* Modify scene*/}
-              <Scene
-                debugShowFramesPerSecond={true}
-              />
+      <div id="cesiumContainer">
+        <Viewer
+          ref={ref => { this.viewerComponent = ref; }}
+          //full
+          resize
+          useBrowserRecommendedResolution
+          animation={false}
+          timeline={false}
+          vrButton={true}
+          terrainProvider={createWorldTerrain({url: IonResource.fromAssetId(1)})}
+          terrainShadows={ShadowMode.CAST_ONLY}
+          //terrainProvider={ new ArcGISTiledElevationTerrainProvider({               url: 'https://landscape6.arcgis.com/arcgis/rest/services/World_Elevation_GMTED/ImageServer/?{AAPK0371b77ce99b4c0d9659b726b31ef861nCekfAet7q4WUOGCvOtSYdVechD-a5JaSp8PNRj7dQ17m1X1FhDfdl8v-nV7VKoA}' }) }
+        >
+          {/* Modify scene*/}
+          <Scene
+            debugShowFramesPerSecond={true}
+          />
 
-              {/*Load Terraperiums:
-                  - For each entity:
-                    - find center and add label
-                    - modify polygon to show based on zoom (function of area)
-                    - modify polygon color based on _, and set base outline color
-                */}
-              <GeoJsonDataSource
-                data={"http://192.168.0.99:5050/Pathic"}
-                onLoad={g => {
-                  const entities = g.entities.values;
+          {/*Load Terraperiums:
+              - For each entity:
+                - find center and add label
+                - modify polygon to show based on zoom (function of area)
+                - modify polygon color based on _, and set base outline color
+            */}
+          <GeoJsonDataSource
+            clampToGround={true}
+            data={"http://192.168.0.99:5050/Pathic"}
+            onLoad={g => {
+              const entities = g.entities.values;
 
-                  for (let i=0; i<entities.length; i++) {
-                    const entity = entities[i];
-                    const center = BoundingSphere.fromPoints(entity.polygon.hierarchy.getValue().positions).center;
+              for (let i=0; i<entities.length; i++) {
+                const entity = entities[i];
+                const center = BoundingSphere.fromPoints(entity.polygon.hierarchy.getValue().positions).center;
 
-                    entity.position = center;
-                    entity.label = {text:entity.name, scaleByDistance:new NearFarScalar(1e5, 0.6, 5e5, 0)};
+                entity.polygon.distanceDisplayCondition = new DistanceDisplayCondition(0,5e5);
+                entity.polygon.material = Color.fromRandom({alpha:0.6});
+                entity.polygon.fill = true;
+                entity.polygon.outline = false;
+                entity.polygon.outlineColor = Color.WHITE;
+                entity.polygon.outlineWidth = 2;
 
-                    entity.polygon.distanceDisplayCondition = new DistanceDisplayCondition(0,5e5);
-                    entity.polygon.material = Color.fromRandom({alpha:0.6});
-                    entity.polygon.fill = true;
-                    entity.polygon.outline = true;
-                    entity.polygon.outlineColor = Color.WHITE;
-                    entity.polygon.outlineWidth = 2;
-                  }
-                }}
-              />
+                entity.position = center;
+                entity.label = {text:entity.name, scaleByDistance:new NearFarScalar(1e5, 0.6, 5e5, 0)};
+              }
+            }}
+          />
 
-              {/*Edit scene to highlight hovered entities*/}
-              <ScreenSpaceEventHandler useDefault>
-                <ScreenSpaceEvent action={this.mouseEvent} type={ScreenSpaceEventType.MOUSE_MOVE} />
-              </ScreenSpaceEventHandler>
-                
-            </Viewer>
-          </Col>
-        </Row>
-      </Container>
+          {/*Edit scene to highlight hovered entities*/}
+          <ScreenSpaceEventHandler useDefault>
+      {/*<ScreenSpaceEvent action={this.mouseEvent} type={ScreenSpaceEventType.MOUSE_MOVE} />*/}
+          </ScreenSpaceEventHandler>
+            
+        </Viewer>
+      </div>
     );
   }
 }
